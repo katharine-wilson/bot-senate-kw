d42136a4ec70ef708f4a7ea56eb8232ee281f34f
Katharine Wilson <152829771+katharine-wilson@users.noreply.github.com>
2025-03-14 21:55:59 +0000

    rough outline

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # bot-senate-kw
+
+I'm planning on making a bot that notifies me every time an action (vote or rule vote) is take non a bill in the U.S. Senate. 
+
+To do this, I will get information from congressional APIs (https://api.congress.gov/#/bill/bill_actions specifically the /bill/{congress}/{billType}/{billNumber}/actions request). 
+
+I would filter to only get information from the Senate chamber and the types of actions I'm interested in (rule vote, passed, failed, signed by president). 
+
+So far, I have made a rough outline for the code for the slack bot. 
---
+++ senate.py
@@ -0,0 +1,24 @@
+import os
+import json
+import requests
+from slack import WebClient
+from slack.errors import SlackApiError
+
+slack_token =
+SenateKey = 
+
+update = f"Update to [bill title/number]: The Senate [passed/failed] a vote on [rule/passage]. To view vote count and text: [url]"
+
+msg = update
+try:
+    response = client.chat_postMessage(
+        channel="slack-bots",
+        text=msg,
+        unfurl_links=True, 
+        unfurl_media=True
+    )
+    print("success!")
+except SlackApiError as e:
+    assert e.response["ok"] is False
+    assert e.response["error"]
+    print(f"Got an error: {e.response['error']}")
