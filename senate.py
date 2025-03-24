ecf76b14642f8f149adc4830e526c955756e9824
Katharine Wilson <152829771+katharine-wilson@users.noreply.github.com>
2025-03-24 20:07:45 +0000

    big updates

--- senate.py
+++ senate.py
@@ -1,28 +1,27 @@
 import os
 import json
 import requests
-from slack import WebClient
-from slack.errors import SlackApiError
+from bs4 import BeautifulSoup
 
-Slack_API=
+url = "https://www.senate.gov/legislative/LIS/roll_call_lists/vote_menu_119_1.xml"
+response = requests.get(url)
 
-SenateKey = "CONGRESS_API_KEY"
+# Check if the request was successful
+if response.status_code == 200:
+    xml_content = response.content
+else:
+    print("Failed to retrieve the XML file.")
 
-url = f"https://api.congress.gov/v3/bill/117/hr/3076/actions?api_key=[SenateKey]"
+soup = BeautifulSoup(xml_content, features = 'xml')
+votes = soup.find_all('vote')
 
-update = f"Update to {first_result['billNumber']}: The Senate [passed/failed] a vote on [passage/rule]. To view vote count and text: [url]"
+for vote in votes:
+    date = vote.find('vote_date').text
+    question = vote.find('question').text
+    issue = vote.find('issue').text
+    yeas = vote.find('yeas').text
+    nays = vote.find('nays').text
 
-msg = update
+update = f"New Senate vote {date}: {question} for [issue]. The yeas were [yeas] and the nays were [nays]. "
 
-try:
-    response = client.chat_postMessage(
-        channel="slack-bots",
-        text=msg,
-        unfurl_links=True, 
-        unfurl_media=True
-    )
-    print("success!")
-except SlackApiError as e:
-    assert e.response["ok"] is False
-    assert e.response["error"]
-    print(f"Got an error: {e.response['error']}")
+print(update)
